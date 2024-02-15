0e8d393a0facaa0ccfb3566bfdc05633d65bfa3d
DedPip <125252491+DedPip@users.noreply.github.com>
2024-02-15 21:51:25 +0330

    Add playRound & playGame & old playRound = round

--- javascript.js
+++ javascript.js
@@ -9,15 +9,17 @@ function getComputerChoice() {
   else {return "getComputerChoice is not working properly"}
 }
 
+// global variables added for each outcome because I wanted to use them in functions other than round like playRound
+const youWin = "You win!";
+const youLose = "You lose!";
+const tie = "It's a tie!";
+
 // we first recieve the two parameters
 // we need to make playerSelection case-insensitive hence the use of playerSelectionLowerCase variable. 
 // finally we write the return value for every possible outcome
-const youWin = "You win!"
-const youLose = "You lose!"
-const tie = "It's a tie!"
-function playRound(playerSelection, computerSelection) {
-  
-  const playerSelectionLowerCase = playerSelection.toLowerCase()
+function round(playerSelection, computerSelection) {
+
+  let playerSelectionLowerCase = playerSelection.toLowerCase();
 
   if (playerSelectionLowerCase === computerSelection) {return tie }
 
@@ -33,5 +35,60 @@ function playRound(playerSelection, computerSelection) {
 
   else if (playerSelectionLowerCase === "scissor" && computerSelection === "paper") {return youWin + " Scissor beats Paper"} 
   
-  else {return "playRound is not working properly"}
+  else {return "round is not working properly"}
 }
+
+// this function plays a single round and returns the result as a string containing one of the following : loss, draw or win
+function playRound() {
+  let playerSelection = prompt("Play with computer: Rock, Paper or Scissor? (5 rounds)");
+  
+  let computerSelection = getComputerChoice();
+
+  let roundResult = round(playerSelection, computerSelection);
+
+  if (roundResult.includes(youLose)) {return "loss"}
+  else if (roundResult.includes(tie)) {return "draw"}
+  else if (roundResult.includes(youWin)) {return "win"}
+  else {return "playRound function is not working properly"}
+}
+
+// 5 rounds are played and each are stored in a variable
+// after all of them are played game results are shown
+// game scores are given and match result is shown
+function playGame() {
+  let wins = 0
+  let loses = 0
+  let round1 = playRound();
+  let round2 = playRound();
+  let round3 = playRound();
+  let round4 = playRound();
+  let round5 = playRound();
+  alert("Game results: " + " " + round1 + ", " +round2 + ", " + round3 + ", " + round4 + " and " + round5)
+
+  if (round1 === "win") {wins = wins + 1;}
+  else if (round1 === "loss") {loses = loses +1;}
+  else {}
+
+  if (round2 === "win") {wins = wins + 1;}
+  else if (round2 === "loss") {loses = loses +1;}
+  else {}
+
+  if (round3 === "win") {wins = wins + 1;}
+  else if (round3 === "loss") {loses = loses +1;}
+  else {}
+
+  if (round4 === "win") {wins = wins + 1;}
+  else if (round4 === "loss") {loses = loses +1;}
+  else {}
+
+  if (round5 === "win") {wins = wins + 1;}
+  else if (round5 === "loss") {loses = loses +1;}
+  else {}
+
+  if (wins > loses) {return "You won the game! congrats"}
+  else if (loses > wins) {return "You lost! better luck next time"}
+  else {return "It's a draw!"}
+  
+}
+
+alert(playGame())
